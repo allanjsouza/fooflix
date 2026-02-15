04cd1ab434acd953bc73a7f33cb259e916d7cf0a
Allan J. Souza <allanjsouza@gmail.com>
2026-02-15 17:43:58 -0300

    feat: connect app with apollo client and fetch movies from database

--- frontend/src/containers/Movies.js
+++ frontend/src/containers/Movies.js
@@ -1,20 +1,27 @@
 import React from "react";
 import Movie from "../components/Movie";
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
 
-const movies = [
-  { name: "John Wick", genre: "Action", year: "2019" },
-  { name: "John Wick", genre: "Action", year: "2019" },
-  { name: "John Wick", genre: "Action", year: "2019" },
-  { name: "John Wick", genre: "Action", year: "2019" },
-  { name: "John Wick", genre: "Action", year: "2019" },
-  { name: "John Wick", genre: "Action", year: "2019" },
-  { name: "John Wick", genre: "Action", year: "2019" },
-];
+const listAllMovies = gql`
+  query {
+    listMovies {
+      name
+      genre
+      year
+    }
+  }
+`;
 
 const Movies = () => {
+  const { loading, error, data } = useQuery(listAllMovies);
+
+  if (loading) return <p className="loading">Loading Movies...</p>;
+  if (error) return <p className="error">Error: {error.message}</p>;
+
   return (
     <div className="movies">
-      {movies.map((movie) => {
+      {data.listMovies.map((movie) => {
         return (
           <Movie
             movieName={movie.name}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
+import App from "./App";
+
+const apolloClient = new ApolloClient({
+  link: new HttpLink({ uri: "http://localhost:4000/graphql" }),
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
+  <ApolloProvider client={apolloClient}>
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>
+  </ApolloProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
