19f2ac33ba50b285fcc82812fd88ca17497d931a
Allan J. Souza <allanjsouza@gmail.com>
2026-02-15 18:32:55 -0300

    feat: allow users to add movies

--- frontend/src/components/AddMovie.js
+++ frontend/src/components/AddMovie.js
@@ -1,13 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import { HashLink as Link } from "react-router-hash-link";
+import { gql } from "@apollo/client";
+import { useMutation } from "@apollo/client/react";
+
+const listAllMovies = gql`
+  query {
+    listMovies {
+      name
+      genre
+      year
+    }
+  }
+`;
+
+const addNewMovie = gql`
+  mutation AddNewMovie($name: String!, $genre: String!, $year: String!) {
+    addMovie(name: $name, genre: $genre, year: $year) {
+      name
+      genre
+      year
+    }
+  }
+`;
 
 const AddMovie = () => {
+  const submitMovie = (e) => {
+    e.preventDefault();
+    newMovieData({
+      variables: { name: movieName, genre: movieGenre, year: movieYear },
+      refetchQueries: [listAllMovies],
+    });
+  };
+
+  const [movieName, setMovieName] = useState("");
+  const [movieGenre, setMovieGenre] = useState("");
+  const [movieYear, setMovieYear] = useState("");
+
+  const [newMovieData, { loading, error }] = useMutation(addNewMovie);
+
+  if (loading) return <p className="loading">"Submitting..."</p>;
+  if (error)
+    return <p className="error">`Submission error! ${error.message}`</p>;
+
   return (
     <div className="addMovie" id="addMovie">
-      <form>
-        <input type="text" placeholder="Movie Name" required />
-        <input type="text" placeholder="Movie Genre" required />
-        <input type="text" placeholder="Movie Year" />
+      <form onSubmit={submitMovie}>
+        <input
+          type="text"
+          placeholder="Movie Name"
+          required
+          onChange={(e) => setMovieName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Movie Genre"
+          required
+          onChange={(e) => setMovieGenre(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Movie Year"
+          onChange={(e) => setMovieYear(e.target.value)}
+        />
         <button>Add Movie</button>
       </form>
       <div className="top">
